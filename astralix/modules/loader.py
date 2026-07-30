2d349f3a3f771b4ee4ad5797e3d0bfc4a1956c45
root <root@lucky-rose.play2go.cloud>
2026-07-30 17:00:20 +0500

    restore original Heroku attribution headers

--- astralix/modules/loader.py
+++ astralix/modules/loader.py
@@ -7,8 +7,8 @@
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
 # ©️ Codrago, 2024-2030
-# This file is a part of astralix Userbot
-# 🌐 https://github.com/coddrago/astralix
+# This file is a part of Heroku Userbot
+# 🌐 https://github.com/coddrago/Heroku
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
